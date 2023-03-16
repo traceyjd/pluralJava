b031e70e3491f61b6255e87baf2be355478d7fcf
Tracey <tracey.tdoherty@gmail.com>
2023-03-16 11:06:25 +0000

    Pluralsight Course

---
+++ src/noteOps.java
@@ -0,0 +1,16 @@
+public class noteOps {
+    public static void main(String[] args) {
+        /* What order is the compiler going to work through the operators
+           Operator Precedence
+           Highest to lowest precedence
+           Postfix Operators - X++ X-- Highest precedence - the increment and decrement operators after a value
+           Prefix Operators - ++X --X before their values
+           Multiplicative Operators - * (multiply) / (divide) % (modulus)
+           Additive operators + (addition) - (subtraction)
+
+
+
+         */
+
+    }
+}
