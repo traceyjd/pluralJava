8f8688488c1da0bd7bdd25ef567f72de21d833b6
Tracey <tracey.tdoherty@gmail.com>
2023-03-16 11:16:10 +0000

    Pluralsight Course

--- src/noteOps.java
+++ src/noteOps.java
@@ -7,10 +7,23 @@ public class noteOps {
            Prefix Operators - ++X --X before their values
            Multiplicative Operators - * (multiply) / (divide) % (modulus)
            Additive operators + (addition) - (subtraction)
+           Just remember when a tie between 2 operators exists it will run left to right
+           You can override precedence by using parenthesis () content of parenthesis is done before the rest
+           You can also have nested parenthesis evaluated from inside out
+           Please take advantage of parenthesis
+           */
+        int valA = 21;
+        int valB = 6;
+        int valC = 3;
+        int valD = 1;
 
+        int result1 = valA - valB / valC;
 
+        int result2 = (valA - valB) / valC;
 
-         */
+        System.out.println(result1);
+        System.out.println(result2);
+
 
     }
 }
